81c971ffe62f78bed8ce2e40c014f21eb69049d5
Fauna <max.maxpinheiro@gmail.com>
2024-01-26 11:49:56 -0300

    0.1.2 - graphical tests with formulas

--- main.c
+++ main.c
@@ -4,20 +4,24 @@ void		*g_mlx;
 
 int	main(int argc, char **argv)
 {
-	t_DNA		*DNA = DNA_new();
-	t_window	*win;
+		t_DNA		*DNA = DNA_new();
+		t_window	*win;
+		t_window	*win2;
 
-	if (argc != 4)
+		if (argc != 4)
 			return (0);
-	g_mlx = mlx_init();
-	win = win_open("gene", 640, 640);
-	draw_parabola(win, vec_new(conv_atoi(argv[1]), conv_atoi(argv[2]), 1, 0),
-					conv_atoi(argv[3]));
-	draw_update(win);
-	put_DNA(DNA);
-	mlx_loop(g_mlx);
-	mlx_destroy_display(g_mlx);
-	free(win);
-	DNA_free(DNA);
-	return (1);
+		g_mlx = mlx_init();
+		win = win_open("sqrt", 640, 640);
+		win2 = win_open("pow2", 640, 640);
+		draw_mth(win, vec_new(conv_atoi(argv[1]), conv_atoi(argv[2]), 1, 0), mth_sqrt);
+		draw_parabola(win2, vec_new(conv_atoi(argv[1]), 320, 1, 0), conv_atoi(argv[3]));
+		draw_update(win);
+		draw_update(win2);
+		put_DNA(DNA);
+		mlx_loop(g_mlx);
+		mlx_destroy_display(g_mlx);
+		free(win);
+		free(win2);
+		DNA_free(DNA);
+		return (1);
 }
