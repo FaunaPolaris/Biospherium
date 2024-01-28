0e28e215160702aeddf0ccdfb1a73bb9caf75aed
Fauna <max.maxpinheiro@gmail.com>
2024-01-28 10:15:20 -0300

    playing with graphics

--- main.c
+++ main.c
@@ -2,26 +2,18 @@
 
 void		*g_mlx;
 
-int	main(int argc, char **argv)
+int	main(void)
 {
 		t_DNA		*DNA = DNA_new();
 		t_window	*win;
-		t_window	*win2;
 
-		if (argc != 4)
-			return (0);
 		g_mlx = mlx_init();
-		win = win_open("sqrt", 640, 640);
-		win2 = win_open("pow2", 640, 640);
-		draw_mth(win, vec_new(conv_atoi(argv[1]), conv_atoi(argv[2]), 1, 0), mth_sqrt);
-		draw_parabola(win2, vec_new(conv_atoi(argv[1]), 320, 1, 0), conv_atoi(argv[3]));
+		win = win_cartesian_table(640, 640);
 		draw_update(win);
-		draw_update(win2);
 		put_DNA(DNA);
 		mlx_loop(g_mlx);
 		mlx_destroy_display(g_mlx);
 		free(win);
-		free(win2);
 		DNA_free(DNA);
 		return (1);
 }
